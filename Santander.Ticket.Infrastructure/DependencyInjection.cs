c5e7e17385528bc57bc729f69ad3732968ab87bf
Vinicius-Rubia <vinicius.deoliveirarubia@gmail.com>
2025-12-18 18:13:16 -0300

    adicionar arquivo de injeção de dependências no projeto de infraestrutura para registrar o DbContext. Adicionar também a connectionString ao projeto

---
+++ Santander.Ticket.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Santander.Ticket.Infrastructure.DataAccess;
+
+namespace Santander.Ticket.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        AddDbContext(services, configuration);
+    }
+
+    private static void AddDbContext(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<SantanderDbContext>(x => x.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+    }
+}
